5fa7d7760764069df6663b5186e4e5f3d9cc3ec3
drxcodev2 <drxcofn2023@gmail.com>
2025-11-09 12:10:15 +0100

    commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -45,7 +45,7 @@ export function BackgroundLinesDemo() {
             <h1 className="gta text-9xl -mt-20 mb-10">GTA VI</h1>
             <h1 className="gta text-9xl -mt-190 lg:-mt-120 blur-2xl mb-10 absolute">GTA VI</h1>        
             <div className="w-auto p-4 border-4 rounded-xl backdrop-blur-3xl bg-white/10 border-white/20">
-                <h2 className="grid grid-cols-1 bg-clip-text gta text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white px-8 text-4xl md:text-4xl lg:text-9xl py-2 md:py-10 relative z-20 tracking-tight space-x-6 lg:px-18 lg:grid-cols-3">
+                <h2 className="grid grid-cols-1 bg-clip-text gta text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white px-8 text-4xl md:text-4xl lg:text-9xl py-2 md:py-10 relative z-20 tracking-tight space-x-2 lg:px-18 lg:grid-cols-4">
                     <span className="text-9xl">{timeleft.days}D</span> <span className="text-9xl">{timeleft.hours}h</span> <span className="text-9xl">{timeleft.minutes}m</span> <span className="text-9xl">{timeleft.seconds}S</span>
                 </h2>
             </div>
